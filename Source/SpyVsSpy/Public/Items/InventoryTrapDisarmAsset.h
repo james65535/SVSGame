83015adb5f334216573b10df01c58cb7b6b38cfa
james65535 <mcafeej@vmware.com>
2025-03-28 18:54:06 +1100

    starting initial work on trap disarming mechanics

---
+++ Source/SpyVsSpy/Public/Items/InventoryTrapDisarmAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/InventoryBaseAsset.h"
+#include "InventoryTrapDisarmAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPYVSSPY_API UInventoryTrapDisarmAsset : public UInventoryBaseAsset
+{
+	GENERATED_BODY()
+	
+};
